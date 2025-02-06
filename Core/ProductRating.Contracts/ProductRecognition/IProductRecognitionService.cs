2676936d44b45d06a2faa1b3d7b8606595024c12
UnknownUser-99 <158577654+UnknownUser-99@users.noreply.github.com>
2025-02-06 04:31:44 +0300

    Product recognition (#1)
    Add product recognition service

---
+++ Core/ProductRating.Contracts/ProductRecognition/IProductRecognitionService.cs
@@ -0,0 +1,9 @@
+﻿using ProductRating.Data.Entities.ProductRecognition;
+
+namespace ProductRating.Contracts.ProductRecognition
+{
+    public interface IProductRecognitionService
+    {
+        RecognitionResult Recognize(byte[] imageBytes);
+    }
+}
---
+++ Core/ProductRating.Data/Configurations/ProductRecognitionServiceOptions.cs
@@ -0,0 +1,9 @@
+﻿namespace ProductRating.Data.Configurations
+{
+    public record ProductRecognitionServiceOptions
+    {
+        public float HighConfidence { get; init; }
+        public float MediumConfidence { get; init; }
+        public float LowConfidence { get; init; }
+    }
+}
