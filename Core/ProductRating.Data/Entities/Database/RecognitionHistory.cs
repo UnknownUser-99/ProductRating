9d1e3d1eff41831478249409b2dc5995e79cc1fa
UnknownUser-99 <158577654+UnknownUser-99@users.noreply.github.com>
2025-02-15 18:55:13 +0300

    Database (#2)
    Add entities from database and context

---
+++ Core/ProductRating.Data/Entities/Database/RecognitionHistory.cs
@@ -0,0 +1,11 @@
+﻿namespace ProductRating.Data.Entities.Database
+{
+    public class RecognitionHistory
+    {
+        public int Id { get; set; }
+        public int Product { get; set; }
+        public int User { get; set; }
+        public string Confidence { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
---
+++ Core/ProductRating.Services/Database/UserService.cs
@@ -0,0 +1,41 @@
+﻿using ProductRating.Contracts.Database;
+using ProductRating.Data.Entities.Database;
+using Microsoft.EntityFrameworkCore;
+
+namespace ProductRating.Services.Database
+{
+    public class UserService : IUserService
+    {
+        private readonly PRDbContext _context;
+
+        public UserService(PRDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<int> AddUserAsync(int phone, string name, string password, string email = null)
+        {
+            User user = new User
+            {
+                Name = name,
+                Phone = phone,
+                Email = email,
+                Password = password
+            };
+
+            UserHistory userHistory = new UserHistory
+            {
+                User = user.Id,
+                Operation = UserOperationType.Register.ToString(),
+                Date = DateTime.UtcNow
+            };
+
+            _context.Users.Add(user);
+            _context.UserHistory.Add(userHistory);
+
+            await _context.SaveChangesAsync();
+
+            return user.Id;
+        }
+    }
+}
