0896e5953e8310ddfefdea3a2dd9178727fb6934
Unknown User <artyomshul@gmail.com>
2025-03-01 17:59:45 +0300

    Add recognition page

---
+++ Hosts/ProductRating.WebApplication/wwwroot/js/recognition.js
@@ -0,0 +1,14 @@
+﻿$(document).ready(function () {
+    $("#imageUpload").on("change", function () {
+        let input = this;
+        let preview = $("#imagePreview");
+
+        if (input.files && input.files[0]) {
+            let reader = new FileReader();
+            reader.onload = function (e) {
+                preview.html('<img src="' + e.target.result + '" class="preview-image">');
+            };
+            reader.readAsDataURL(input.files[0]);
+        }
+    });
+});
