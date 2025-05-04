ec430bda82e79fc5c140fe73bf899d88a47a0ea3
Unknown User <artyomshul@gmail.com>
2025-05-04 21:22:45 +0300

    Add configuration API url.

---
+++ Core/ProductRating.Data/Configurations/UrlAPIOptions.cs
@@ -0,0 +1,10 @@
+﻿namespace ProductRating.Data.Configurations
+{
+    public record UrlAPIOptions
+    {
+        public string AuthRequest { get; init; }
+        public string ProductRecognitionRequest { get; init; }
+        public string ProductRequest { get; init; }
+        public string ReviewRequest { get; init; }
+    }
+}
